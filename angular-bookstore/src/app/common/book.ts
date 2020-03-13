49c049b0e0c82226fb725f9d90423ab4dd0d0d5d
umeshpedde <umesh.pedde@neutrinotechlabs.com>
2020-03-14 03:01:03 +0530

    created angular project and completed feature 1.

---
+++ angular-bookstore/src/app/common/book.ts
@@ -0,0 +1,14 @@
+export class Book {
+
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: string;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    createdOn: Date;
+    updatedOn: Date;
+}
+
+
--- onlinebookstore/src/main/java/com/neutrino/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/neutrino/onlinebookstore/repository/BookRepository.java
@@ -1,8 +1,11 @@
 package com.neutrino.onlinebookstore.repository;
 
-import com.neutrino.onlinebookstore.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+import com.neutrino.onlinebookstore.entity.Book;
+
+@CrossOrigin("*")
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
